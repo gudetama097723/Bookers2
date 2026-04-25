230f7c826e7c3d7a5fc629054edcb755dc3a7549
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-04-25 09:39:16 +0000

    フォロー、フォロワー一覧実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.followings
+  end
 
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+  end
 
   private
 
