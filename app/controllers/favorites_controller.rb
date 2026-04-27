c516d29599a6eb4ad751da37e00d9344297d3e53
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-04-27 10:52:19 +0000

    いいね非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,14 @@
 class FavoritesController < ApplicationController
-  def create
-    book = Book.find(params[:book_id])
-    favorite = Current.user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_back fallback_location: books_path
-  end
+def create
+  @book = Book.find(params[:book_id])
+  Current.user.favorites.create!(book: @book)
+  respond_to(&:turbo_stream)
+end
 
-  def destroy
-    book = Book.find(params[:book_id])
-    favorite = Current.user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_back fallback_location: books_path
-  end
+def destroy
+  @book = Book.find(params[:book_id])
+  Current.user.favorites.find_by(book: @book)&.destroy
+  respond_to(&:turbo_stream)
+end
 
 end
