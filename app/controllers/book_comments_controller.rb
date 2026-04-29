e2d748612320db0d4bc45a2ff35645eb2aec9452
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-04-29 06:34:59 +0000

    コメント機能非同期通信成功

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,30 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = Current.user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.build(book_comment_params)
+    @book_comment.user = Current.user
+
+    if @book_comment.save
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to book_path(@book)}
+    end
+    else
+    @book = Book.find(params[:book_id])
+    render "books/show"
+    end
   end
 
   def destroy
-  book = Book.find(params[:book_id])
-  comment = BookComment.find(params[:id])
-  comment.destroy
-  redirect_to book_path(book)
+  @book = Book.find(params[:book_id])
+  @book_comment = BookComment.find(params[:id])
+  @book_comment.destroy
+
+  respond_to do |format|
+    format.turbo_stream
+    format.html { redirect_to book_path(@book)}
+  end
   end
 
   private
