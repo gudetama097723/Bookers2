bdddbeec0ec6995108611548946025b3b39c1735
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-04-29 07:22:49 +0000

    いいね非同期通信完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   before_action :set_book, only: [:create, :destroy]
   def create
     @favorite = @book.favorites.create(user: Current.user)
-
+    @book.reload
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_to request.referrer}
@@ -12,7 +12,7 @@ class FavoritesController < ApplicationController
   def destroy
     @favorite = @book.favorites.find_by(user: Current.user)
     @favorite.destroy if @favorite
-
+        @book.reload
     respond_to do |format|
       format.turbo_stream
       format.html {redirect_to request.referrer}
